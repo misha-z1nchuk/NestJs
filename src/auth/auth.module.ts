8467a99a8097b969ea27e8018613544dfe6aa5d6
Misha <zinchuckmisha@ukr.net>
2022-01-30 13:04:49 +0200

    Auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import {UsersModule} from "../users/users.module";
@@ -13,13 +13,17 @@ import {ConfigModule} from "@nestjs/config";
       ConfigModule.forRoot({
           envFilePath: `.${process.env.NODE_ENV}.env`,
       }),
-      UsersModule,
+      forwardRef(() => UsersModule),
       JwtModule.register({
           secret: process.env.PRIVATE_KEY,
           signOptions: {
               expiresIn: '24h'
           }
       })
-  ]
+  ],
+    exports: [
+        AuthService,
+        JwtModule
+    ]
 })
 export class AuthModule {}
